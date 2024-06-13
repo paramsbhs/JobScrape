6375a1f0418ca37c641a2c85dac86e567506b5be
Parampreet Singh <params@uvic.ca>
2024-06-13 00:19:42 -0700

    card error

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ def get_url(position):
 url = get_url('software')
 
 response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+cards = soup.find_all('div', 'searchResult')
+
+card = cards[0]
+atag = card.h2.a
+job_title = atag.get('title')
+
+job_url = 'https://learninginmotion.uvic.ca' + atag.get('href')
+
+company = card.find('span', {"class": "overflowToTitle"}).text.strip()
