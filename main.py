2f28339036e9578a6f937109559cd66c4cd8a077
Parampreet Singh <params@uvic.ca>
2024-06-13 00:26:26 -0700

    fixed bugs

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-import csv
-from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+# URL of the job postings page
+def get_url():
+    return "https://learninginmotion.uvic.ca/myAccount/co-op/postings.htm"
 
-def get_url(position):
-    template = "https://learninginmotion.uvic.ca/myAccount/co-op/postings.htm"
-    url = template.format(position)
-    return url
-
-url = get_url('software')
+url = get_url()
 
 response = requests.get(url)
+
 soup = BeautifulSoup(response.text, 'html.parser')
-cards = soup.find_all('div', 'searchResult')
 
-card = cards[0]
-atag = card.h2.a
-job_title = atag.get('title')
 
-job_url = 'https://learninginmotion.uvic.ca' + atag.get('href')
+cards = soup.find_all('div', class_='searchResult')
 
-company = card.find('span', {"class": "overflowToTitle"}).text.strip()
+
+jobs = []
+for card in cards:
+    atag = card.find('a')
+    job_title = atag.get('title')
+    job_url = 'https://learninginmotion.uvic.ca' + atag.get('href')
+    company = card.find('span', class_='overflowToTitle').text.strip()
+    
+    jobs.append({
+        'job_title': job_title,
+        'job_url': job_url,
+        'company': company
+    })
+
+for job in jobs:
+    print(f"Job Title: {job['job_title']}")
+    print(f"Company: {job['company']}")
+    print(f"Job URL: {job['job_url']}")
+    print('-' * 20)
